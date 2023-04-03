2c61cb117f777d4f6e9bfc1b154fbb2e54a3acdf
eesuhn <102596628+eesuhn@users.noreply.github.com>
2023-04-03 18:05:46 +0800

    Update database.sql
    1. Added food table

--- database.sql
+++ database.sql
@@ -13,12 +13,33 @@ CREATE TABLE `currency` (
     `currencyNum` int(11) NOT NULL
 );
 
+-- food table
+CREATE TABLE `food` (
+    `foodID` int(11) NOT NULL,
+    `foodImg` varchar(255) NOT NULL,
+    `foodName` varchar(255) NOT NULL,
+    `foodDesc` varchar(255) NOT NULL,
+    `foodPrice` int(11) NOT NULL,
+    `foodHunger` int(11) NOT NULL,
+    `foodXP` int(11) NOT NULL
+);
+
+-- food_inventory table
+CREATE TABLE `food_inventory` (
+    `userID` int(11) NOT NULL,
+    `foodID` int(11) NOT NULL,
+    `foodNum` int(11) NOT NULL
+);
+
 -- Primary key
 ALTER TABLE `user`
     ADD PRIMARY KEY (`userID`);
 
 ALTER TABLE `currency`
     ADD PRIMARY KEY (`currencyID`);
+
+ALTER TABLE `food`
+    ADD PRIMARY KEY (`foodID`);
 
 -- Auto increment
 ALTER TABLE `user`
@@ -27,6 +48,15 @@ ALTER TABLE `user`
 ALTER TABLE `currency`
     MODIFY `currencyID` int(11) NOT NULL AUTO_INCREMENT;
 
+ALTER TABLE `food`
+    MODIFY `foodID` int(11) NOT NULL AUTO_INCREMENT;
+
 -- Foreign key
 ALTER TABLE `currency`
     ADD CONSTRAINT `currency_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+ALTER TABLE `food_inventory`
+    ADD CONSTRAINT `food_inventory_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+ALTER TABLE `food_inventory`
+    ADD CONSTRAINT `food_inventory_ibfk_2` FOREIGN KEY (`foodID`) REFERENCES `food` (`foodID`);

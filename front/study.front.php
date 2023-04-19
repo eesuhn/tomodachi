4196636ec091bffeaf09c0a20a5a08c05385e88b
eesuhn <102596628+eesuhn@users.noreply.github.com>
2023-04-19 12:39:38 +0800

    Update title

--- front/study.front.php
+++ front/study.front.php
@@ -9,7 +9,7 @@ if (session_status() == PHP_SESSION_NONE) {
 <html>
 
 <head>
-    <title>Tomodachi || Habit Tracker</title>
+    <title>Tomodachi | Habit Tracker</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
--- front/welcome.front.php
+++ front/welcome.front.php
@@ -16,7 +16,7 @@
 <html>
 
   <head>
-    <title>Tomodachi || Habit Tracker</title>
+    <title>Tomodachi | Habit Tracker</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
